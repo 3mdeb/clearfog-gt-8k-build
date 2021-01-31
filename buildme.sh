421a19b76674cbdc397ff4f147be79563016672c
Maciej Pijanowski <maciej.pijanowski@3mdeb.com>
2021-01-31 21:10:59 +0100

    wip
    Signed-off-by: Maciej Pijanowski <maciej.pijanowski@3mdeb.com>

--- buildme.sh
+++ buildme.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 echo "Checking all required tools are installed"
 TOOLS="wget tar git make 7z unsquashfs dd mkfs.ext4 parted dtc losetup patch"
@@ -35,6 +35,11 @@ export BINARIES_BRANCH=binaries-marvell-armada-18.12
 
 # Marvell ATF
 export ATF_BRANCH=atf-v1.5-armada-18.12
+
+# # mbedtls config (dependency for the secure ATF build)
+# export MBEDTLSDIR=mbedtls
+# export MBEDTLS_REPO=git@github.com:ARMmbed/mbedtls.git
+# export MBEDTLS_TAG=mbedtls-2.8.0
 
 # Marvell DDR
 export MVDDR_BRANCH=mv_ddr-armada-18.12
@@ -88,20 +93,29 @@ else
         git branch -v
 fi
 
-echo "Downoading Marvell ATF"
-if [[ ! -d $ROOTDIR/build/bootloader/atf-marvell ]]; then
-	cd $ROOTDIR/build/bootloader
-	git clone --branch=$ATF_BRANCH --depth=1 https://github.com/MarvellEmbeddedProcessors/atf-marvell.git
-	cd atf-marvell
-else
-	cd $ROOTDIR/build/bootloader/atf-marvell
-	git reset
-	git checkout .
-	git clean -fdx
-	git pull origin $ATF_BRANCH
-	git branch -v
-fi
-for n in $ROOTDIR/patches/atf/*.patch; do patch -p1 -i $n; done
+# echo "Downoading Marvell ATF"
+# if [[ ! -d $ROOTDIR/build/bootloader/atf-marvell ]]; then
+# 	cd $ROOTDIR/build/bootloader
+# 	git clone --branch=$ATF_BRANCH --depth=1 https://github.com/MarvellEmbeddedProcessors/atf-marvell.git
+# 	cd atf-marvell
+# else
+# 	cd $ROOTDIR/build/bootloader/atf-marvell
+# 	git reset
+# 	git checkout .
+# 	git clean -fdx
+# 	git pull origin $ATF_BRANCH
+# 	git branch -v
+# fi
+# for n in $ROOTDIR/patches/atf/*.patch; do patch -p1 -i $n; done
+
+# echo "Downoading mbedtls"
+# if [[ ! -d $ROOTDIR/build/bootloader/$MBEDTLSDIR ]]; then
+# 	cd $ROOTDIR/build/bootloader
+# 	git clone $MBEDTLS_REPO
+# 	cd $MBEDTLSDIR
+# 	git fetch --tags
+# 	git checkout -b $MBEDTLS_TAG $MBEDTLS_TAG
+# fi
 
 echo "Downloading Marvell DDR"
 if [[ ! -d $ROOTDIR/build/bootloader/mv-ddr-marvell ]]; then
@@ -135,81 +149,81 @@ if [ $? != 0 ]; then
 	exit -1
 fi
 
-echo "Downloading Linux kernel"
-if [[ ! -d $ROOTDIR/build/$KERNELDIR ]]; then
-	cd $ROOTDIR/build/
-	git clone --branch=$KERNEL_BRANCH $KERNEL_REPO $KERNELDIR
-	cd $KERNELDIR
-else
-	cd $ROOTDIR/build/$KERNELDIR
-	git reset 
-	git checkout .
-	git clean -fdx
-	git pull origin $KERNEL_BRANCH
-	git branch -v
-fi
-for n in $ROOTDIR/patches/kernel/*.patch; do patch -p1 -i $n; done
-
-echo "Building Linux Kernel"
-cd $ROOTDIR/build/$KERNELDIR
-make defconfig
-./scripts/kconfig/merge_config.sh .config $ROOTDIR/configs/kernel.extra.config
-make -j4
-if [ $? != 0 ]; then
-	echo "Error building kernel"
-	exit -1
-fi
-
-echo "Downloading Ubuntu Image"
-if [[ ! -f $ROOTDIR/build/ubuntu-$UBUNTU_VER-server-arm64.squashfs ]]; then
-        cd $ROOTDIR/build
-        if [[ ! -f ubuntu-$UBUNTU_VER-server-arm64.iso ]]; then
-                wget http://cdimage.ubuntu.com/releases/18.04/release/ubuntu-$UBUNTU_VER-server-arm64.iso
-        fi
-        7z x ubuntu-$UBUNTU_VER-server-arm64.iso install/filesystem.squashfs
-	mv install/filesystem.squashfs ubuntu-$UBUNTU_VER-server-arm64.squashfs
-fi
-
-cd $ROOTDIR
-
-echo "Creating partitions and images"
-dd if=/dev/zero of=$ROOTDIR/image.img bs=1M count=512
-parted --script -a optimal $ROOTDIR/image.img mklabel msdos mkpart primary 4096s 100% set 1 boot on
-
-echo "Filling image with data"
-mkdir -pv $ROOTDIR/image
-LOOPDEV=`losetup -f`
-${SUDO}losetup -o 2097152 $LOOPDEV $ROOTDIR/image.img
-${SUDO}mkfs.ext4 $LOOPDEV
-${SUDO}mount $LOOPDEV $ROOTDIR/image
-
-echo "Copying filesystem to the image"
-${SUDO}unsquashfs -d $ROOTDIR/image/ -f $ROOTDIR/build/ubuntu-$UBUNTU_VER-server-arm64.squashfs
-
-echo "Copying kernel to the image"
-cp -av $ROOTDIR/build/$KERNELDIR/arch/arm64/boot/Image $ROOTDIR/image/boot/
-cp -av $ROOTDIR/build/$KERNELDIR/arch/arm64/boot/dts/marvell/armada-8040-clearfog-gt-8k.dtb $ROOTDIR/image/boot/
-cat > $ROOTDIR/image/boot.txt <<EOF
-setenv earlyprintk kpti=0 swiotlb=0 console=ttyS0,115200 root=/dev/mmcblk1p1 net.ifnames=0 biosdevname=0 fsck.mode=auto fsck.repair=yes rootwait ro
-load ${devtype} ${devnum} ${kernel_addr_r} /boot/Image
-load ${devtype} ${devnum} ${fdt_addr_r} /boot/armada-8040-clearfog-gt-8k.dtb
-booti ${kernel_addr_r} - ${fdt_addr_r}
-EOF
-$ROOTDIR/build/bootloader/$UBOOTDIR/tools/mkimage -A arm64 -T script -O linux -d $ROOTDIR/image/boot.txt $ROOTDIR/image/boot.scr
-cd $ROOTDIR/build/$KERNELDIR && make INSTALL_MOD_PATH=$ROOTDIR/image/ INSTALL_MOD_STRIP=1 modules_install
-${SUDO}chown -R root:root $ROOTDIR/image/boot $ROOTDIR/image/lib/modules
-
-cd $ROOTDIR/image
-${SUDO}patch -p1 -i $ROOTDIR/patches/rootfs/01-fstab.patch
-${SUDO}patch -p1 -i $ROOTDIR/patches/rootfs/02-autologin-on-serial-console.patch
-cd $ROOTDIR
-
-echo "Finishing..."
-${SUDO}umount $ROOTDIR/image
-${SUDO}losetup -d $LOOPDEV
-
-echo "Fusing bootloader to the image"
-dd if=$ROOTDIR/build/bootloader/atf-marvell/build/a80x0_cf_gt_8k/release/flash-image.bin of=image.img conv=notrunc bs=512 seek=1
+# echo "Downloading Linux kernel"
+# if [[ ! -d $ROOTDIR/build/$KERNELDIR ]]; then
+# 	cd $ROOTDIR/build/
+# 	git clone --branch=$KERNEL_BRANCH $KERNEL_REPO $KERNELDIR
+# 	cd $KERNELDIR
+# else
+# 	cd $ROOTDIR/build/$KERNELDIR
+# 	git reset 
+# 	git checkout .
+# 	git clean -fdx
+# 	git pull origin $KERNEL_BRANCH
+# 	git branch -v
+# fi
+# for n in $ROOTDIR/patches/kernel/*.patch; do patch -p1 -i $n; done
+# 
+# echo "Building Linux Kernel"
+# cd $ROOTDIR/build/$KERNELDIR
+# make defconfig
+# ./scripts/kconfig/merge_config.sh .config $ROOTDIR/configs/kernel.extra.config
+# make -j$(nproc)
+# if [ $? != 0 ]; then
+# 	echo "Error building kernel"
+# 	exit -1
+# fi
+# 
+# echo "Downloading Ubuntu Image"
+# if [[ ! -f $ROOTDIR/build/ubuntu-$UBUNTU_VER-server-arm64.squashfs ]]; then
+#         cd $ROOTDIR/build
+#         if [[ ! -f ubuntu-$UBUNTU_VER-server-arm64.iso ]]; then
+#                 wget http://cdimage.ubuntu.com/releases/18.04/release/ubuntu-$UBUNTU_VER-server-arm64.iso
+#         fi
+#         7z x ubuntu-$UBUNTU_VER-server-arm64.iso install/filesystem.squashfs
+# 	mv install/filesystem.squashfs ubuntu-$UBUNTU_VER-server-arm64.squashfs
+# fi
+# 
+# cd $ROOTDIR
+# 
+# echo "Creating partitions and images"
+# dd if=/dev/zero of=$ROOTDIR/image.img bs=1M count=512
+# parted --script -a optimal $ROOTDIR/image.img mklabel msdos mkpart primary 4096s 100% set 1 boot on
+# 
+# echo "Filling image with data"
+# mkdir -pv $ROOTDIR/image
+# LOOPDEV=`losetup -f`
+# ${SUDO}losetup -o 2097152 $LOOPDEV $ROOTDIR/image.img
+# ${SUDO}mkfs.ext4 $LOOPDEV
+# ${SUDO}mount $LOOPDEV $ROOTDIR/image
+# 
+# echo "Copying filesystem to the image"
+# ${SUDO}unsquashfs -d $ROOTDIR/image/ -f $ROOTDIR/build/ubuntu-$UBUNTU_VER-server-arm64.squashfs
+# 
+# echo "Copying kernel to the image"
+# cp -av $ROOTDIR/build/$KERNELDIR/arch/arm64/boot/Image $ROOTDIR/image/boot/
+# cp -av $ROOTDIR/build/$KERNELDIR/arch/arm64/boot/dts/marvell/armada-8040-clearfog-gt-8k.dtb $ROOTDIR/image/boot/
+# cat > $ROOTDIR/image/boot.txt <<EOF
+# setenv earlyprintk kpti=0 swiotlb=0 console=ttyS0,115200 root=/dev/mmcblk1p1 net.ifnames=0 biosdevname=0 fsck.mode=auto fsck.repair=yes rootwait ro
+# load ${devtype} ${devnum} ${kernel_addr_r} /boot/Image
+# load ${devtype} ${devnum} ${fdt_addr_r} /boot/armada-8040-clearfog-gt-8k.dtb
+# booti ${kernel_addr_r} - ${fdt_addr_r}
+# EOF
+# $ROOTDIR/build/bootloader/$UBOOTDIR/tools/mkimage -A arm64 -T script -O linux -d $ROOTDIR/image/boot.txt $ROOTDIR/image/boot.scr
+# cd $ROOTDIR/build/$KERNELDIR && make INSTALL_MOD_PATH=$ROOTDIR/image/ INSTALL_MOD_STRIP=1 modules_install
+# ${SUDO}chown -R root:root $ROOTDIR/image/boot $ROOTDIR/image/lib/modules
+# 
+# cd $ROOTDIR/image
+# ${SUDO}patch -p1 -i $ROOTDIR/patches/rootfs/01-fstab.patch
+# ${SUDO}patch -p1 -i $ROOTDIR/patches/rootfs/02-autologin-on-serial-console.patch
+# cd $ROOTDIR
+# 
+# echo "Finishing..."
+# ${SUDO}umount $ROOTDIR/image
+# ${SUDO}losetup -d $LOOPDEV
+# 
+# echo "Fusing bootloader to the image"
+# dd if=$ROOTDIR/build/bootloader/atf-marvell/build/a80x0_cf_gt_8k/release/flash-image.bin of=image.img conv=notrunc bs=512 seek=1
 
 echo "Done."
 cd $ROOTDIR
